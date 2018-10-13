6b804d65592958a664c8f5f93aa8e3df5b0c6f77
rafaeltapia <rtapia97>
2018-10-13 18:41:19 -0300

    Alterado a forma de exibicao ao iniciar a API

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 import { Server } from './server/server';
 import { usersRouter } from './users/users.router';
+import { customersRouter } from './customers/customers.router';
 import { mainRouter } from './main.router';
 
 const server = new Server();
 server.bootstrap([
   usersRouter,
+  customersRouter,
   mainRouter
 ]).then(server => {
-  console.log('Server is listening on: ', server.application.address());
+  console.log('\n#################### Server is alive! ####################\n');
+  console.log('==========================================================');
+  console.log('Server is listening on:\n ', server.application.address());
+  console.log('==========================================================');
 }).catch(error => {
   console.log('Server failed to start');
   console.log(error);
