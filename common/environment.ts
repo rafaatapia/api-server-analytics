e61523420b05aba56db49131c68f15fdf7cc4237
rafaeltapia <rtapia97>
2018-11-05 21:13:04 -0200

    Aumentado numero de registro por página

--- common/environment.ts
+++ common/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   server: { port: process.env.SERVER_PORT || 3000 },
   db: { url: process.env.DB_URL || "mongodb://localhost/bmake" },
   security: {
-    saltRounds: process.env.SALT_ROUNDS || 10,
+    saltRounds: process.env.SALT_ROUNDS || 999999,
     apiSecret: process.env.API_SECRET || 'bmake-secret',
     enableHTTPS: process.env.ENABLE_HTTPS || false,
     certificate: process.env.CERTI_FILE || './security/keys/cert.pem',
